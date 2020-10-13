aa6d80a38cbe19077b0d611344af0c263b93bbf0
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2020-10-12 23:23:19 -0500

    Se creo el primer componente HolaMundo.js

---
+++ src/components/HolaMundo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//El nombre de la funcion debe ser igual al nombre del componente
+//Debe iniciar en mayusculas
+function HolaMundo(){
+
+    return(
+        <div>
+            <h1>Hola Mundo!!!</h1>
+            <h2>Mi primer componente creado con ReactJS</h2>  
+        </div>
+    );
+}
+
+//El componente debe exportarse
+export default HolaMundo;
