70aef8d8875e74cb3bfe59e8861e2a901e85b0a9
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2020-10-12 23:25:36 -0500

    Primer componente

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HolaMundo, { Adios } from "./components/HolaMundo"
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <HolaMundo />
+        <Adios />
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/HolaMundo.js
+++ src/components/HolaMundo.js
@@ -2,15 +2,29 @@ import React from "react";
 
 //El nombre de la funcion debe ser igual al nombre del componente
 //Debe iniciar en mayusculas
-function HolaMundo(){
+export default function HolaMundo(){
 
+    //return solo puede retornar un objeto
+    //el div, contiene otros elementos dentro pero al final es un solo objeto div
+    //Se puede utilizar fragment si no se quiere usar el <div>....</div>, <> ... </>
     return(
         <div>
             <h1>Hola Mundo!!!</h1>
             <h2>Mi primer componente creado con ReactJS</h2>  
+            <h3>Realizando pruebas...</h3>
         </div>
     );
 }
 
-//El componente debe exportarse
-export default HolaMundo;
+//Nuevo Componente
+//Lo correcto es que cada componente esté por separado, Adios.js dentro del directorio components
+export function Adios(){
+    return(
+        <div>
+            <h4>Adios....</h4>
+        </div>
+    );
+}
+
+//El componente debe exportarse como se realizó o bien...
+//export default HolaMundo;
